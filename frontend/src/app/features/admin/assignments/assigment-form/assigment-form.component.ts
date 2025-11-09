5459f9bc9a5149e52fbb02832e56f8e6ea40d71c
Hermes Castro <161852864+IngHermesCastro@users.noreply.github.com>
2025-11-08 23:17:10 -0500

    Componentes principales

---
+++ frontend/src/app/features/admin/assignments/assigment-form/assigment-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assigment-form',
+  imports: [],
+  templateUrl: './assigment-form.component.html',
+  styleUrl: './assigment-form.component.css'
+})
+export class AssigmentFormComponent {
+
+}
